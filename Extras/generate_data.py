5c4b9e4bc9719f81b23526365bacdd3509cc6346
Alaninja1209 <alanalfre.2001@gmail.com>
2023-06-21 19:30:23 -0600

    Pickachu

--- Extras/generate_data.py
+++ Extras/generate_data.py
@@ -1,3 +1,28 @@
 import cv2 as cv
 import os
 
+output_folder = 'Extras\data'
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+face_cascade = cv.CascadeClassifier(r'Advance_Scripts\cascade\haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier(r'Advance_Scripts\cascade\haarcascade_eye.xml')
+
+camera = cv.VideoCapture(0)
+count = 0
+
+while (cv.waitKey(1) == -1):
+    success, frame = camera.read()
+
+    if success:
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(120, 120))
+        for (x, y, w, h) in faces:
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            face_img = cv.resize(gray[y:y+h, x:x+w], (200, 200))
+            face_filename = '%s/%d.pgm' % (output_folder, count)
+            cv.imwrite(face_filename, face_img)
+            count += 1
+
+        cv.imshow('Capturing faces...', frame)
